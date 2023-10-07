751491fbc7385547e4ee7ade73714eb2b1eacc33
Kelvin Alex <lilkevinrobert@gmail.com>
2023-10-07 03:42:12 +0300

    Updated tsconfig file

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Banner from '../components/Banner/Banner';
-import NavBar from '../components/Navbar/Navbar';
 import Nominees from '../components/Sections/Nominees';
 import Footer from '../components/Footer/Footer';
+import NavBar from "../components/navbar/NavBar";
 
 const HomePage = () => {
   return (
