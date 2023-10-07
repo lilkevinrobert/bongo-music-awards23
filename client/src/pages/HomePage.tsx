7a5ab3c8ef09dd8fd7fcaf844baa0b6d161e9611
Kelvin Alex <lilkevinrobert@gmail.com>
2023-10-07 03:49:01 +0300

    Updated the tsconfig file

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import Banner from '../components/Banner/Banner';
-import Nominees from '../components/Sections/Nominees';
 import Footer from '../components/Footer/Footer';
 import NavBar from "../components/navbar/NavBar";
 
