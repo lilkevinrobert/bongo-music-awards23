789ce7586c891a24959af456d6d3acaed8cd5713
cmn-lab <cnguruka2@gmail.com>
2023-10-20 13:55:37 +0300

    style: Improve about page UI

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -5,9 +5,9 @@ const About = () => {
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center gap-4 lg:gap-6 py-10 lg:px-36">
       <section className="w-full px-8 lg:px-28 flex flex-col items-center justify-center gap-4">
-        <Typography variant="h6" className="underline underline-offset-4">About Us</Typography>
+        <Typography variant="h6" className="underline underline-offset-4 text-lg">About Us</Typography>
         <Typography>
-          Welcome to the {awardSiteName} official web page, your ultimate
+          Welcome to the <span className="bg-yellow-400 p-1 text-white"> {awardSiteName} </span> official web page, your ultimate
           destination for celebrating and recognizing the exceptional talents
           that shape the world of music. We are a passionate team of music
           enthusiasts, artists, and industry professionals who believe in the
@@ -15,7 +15,7 @@ const About = () => {
         </Typography>
       </section>
       <section className="w-full px-8 lg:px-28 flex flex-col items-center justify-center gap-4">
-        <Typography variant="h6" className="underline underline-offset-4" >Our Mission</Typography>
+        <Typography variant="h6" className="underline underline-offset-4 text-lg" >Our Mission</Typography>
         <Typography>
           Our Mission At {awardSiteName} is simple: to
           honor and celebrate the artists, musicians, and creative visionaries
@@ -25,7 +25,7 @@ const About = () => {
         </Typography>
       </section>
       <section className="w-full px-8 lg:px-28 flex flex-col items-center justify-center gap-4">
-        <Typography variant="h6" className="underline underline-offset-4" >What we Do</Typography>
+        <Typography variant="h6" className="underline underline-offset-4 text-lg" >What we Do</Typography>
         <div className="gap-4 flex flex-col items-center justify-center">
           <li> Awards Ceremony: Each year, we host a prestigious music
           awards ceremony that showcases the best in the industry. We recognize
@@ -46,11 +46,11 @@ const About = () => {
         </div>
       </section>
       <section className="w-full px-8 lg:px-28 flex flex-col items-center justify-center gap-4">
-        <Typography variant="h6" className="underline underline-offset-4">Our Team</Typography>
+        <Typography variant="h6" className="underline underline-offset-4 text-lg">Our Team</Typography>
         <Typography>
           Our Team is composed of music industry experts, event
           organizers, tech-savvy professionals, and dedicated music enthusiasts.
-          Together, we work tirelessly to ensure that <span className="underline underline-offset-4">{awardSiteName} </span> is a platform that reflects the ever-evolving and vibrant world
+          Together, we work tirelessly to ensure that <span className="underline underline-offset-4 bg-yellow-400 p-1 text-white">{awardSiteName} </span> is a platform that reflects the ever-evolving and vibrant world
           of music.
         </Typography>
       </section>
