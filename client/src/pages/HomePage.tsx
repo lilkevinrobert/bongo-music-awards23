5175ac2fce6aca885789d5a6e7d6849930fdd749
cmn-lab <cnguruka2@gmail.com>
2023-10-23 23:05:04 +0300

    fix: Correct navbar import

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import Banner from '../components/Banner/Banner';
 import Footer from '../components/Footer/Footer';
-import NavBar from "../components/navbar/NavBar";
+import NavBar from "../components/Navbar/NavBar";
 
 const HomePage = () => {
   return (
