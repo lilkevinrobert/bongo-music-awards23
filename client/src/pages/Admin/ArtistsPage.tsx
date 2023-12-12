8cef8bddc3d40e45049db7aea8e47670d009a628
cmn-lab <cnguruka2@gmail.com>
2023-12-12 22:15:16 +0300

    feat: Add navigation to dashboard on breadcrumb

--- client/src/pages/Admin/ArtistsPage.tsx
+++ client/src/pages/Admin/ArtistsPage.tsx
@@ -8,7 +8,7 @@ const ArtistsPage = () => {
     <Layout>
       <div className=" text-slate-900 py-2">
         <Breadcrumbs separator="-">
-          <NavLink to="#" className="opacity-60 pr-2">
+          <NavLink to="../dashboard" className="opacity-60 pr-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-4 w-4"
@@ -18,7 +18,7 @@ const ArtistsPage = () => {
               <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
             </svg>
           </NavLink>
-          <NavLink to="#" className="opacity-60 px-2">
+          <NavLink to="../dashboard" className="opacity-60 px-2">
             <span>Admin</span>
           </NavLink>
           <span className="px-2">Artists</span>
