f955f4589be44dd36cb2828a50b64100ed5a243c
cmn-lab <cnguruka2@gmail.com>
2024-02-16 22:53:07 +0300

    style: Improve UI for the recovery page

--- client/src/pages/RecoveryPage.tsx
+++ client/src/pages/RecoveryPage.tsx
@@ -1,9 +1,41 @@
-const RecoveryPage = () => {
-    return (
-        <>
-        recovery page
-        </>
-    )
-}
+import { Button, Input, Typography } from "@material-tailwind/react";
+import { NavLink } from "react-router-dom";
 
-export default RecoveryPage
+const RecoveryPage = () => {
+  const getYear = () => {
+    const date = new Date();
+    return date.getFullYear();
+  };
+  return (
+    <div className="w-screen h-screen bg-white py-4 px-6 flex items-center justify-center">
+      <div className="text-slate-950">
+        <div className="pb-4">
+          <Typography className="text-2xl text-center text-slate-900">
+            Bongo <span className="text-yellow-400"> Music Awards</span>
+          </Typography>
+          <Typography className="text-center text-sm font-light">
+            Password Recovery
+          </Typography>
+        </div>
+        <form className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <Typography>Email</Typography>
+            <Input
+              placeholder="Enter your recovery email"
+              crossOrigin={undefined}
+              className="rounded-md"
+            />
+          </div>
+          <Button className="transition ease-in-out font-bold hover:text-slate-900 hover:bg-yellow-400">
+            Proceed
+          </Button>
+        </form>
+        <Typography className="text-center text-xs font-light text-slate-400 py-4">
+          &copy; {getYear()}
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default RecoveryPage;
