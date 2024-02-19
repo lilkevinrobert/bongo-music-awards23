0f5d0bf47e8d12e384621c59ec39463af3c234f5
cmn-lab <cnguruka2@gmail.com>
2024-02-19 15:42:46 +0300

    style: Add breadcrumb level 1 component

---
+++ client/src/components/Breadcrumbs/BreadcrumbLevel1.tsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+import { Breadcrumbs, Typography } from "@material-tailwind/react";
+import { AiFillHome } from "react-icons/ai";
+
+interface BreadcrumbLevel1Props {
+    currentPage: string;
+}
+
+const BreadcrumbLevel1 = ({ currentPage }:BreadcrumbLevel1Props) => {
+  return (
+    <div className=" text-slate-900 pb-2">
+      <Breadcrumbs separator="-">
+        <NavLink to="../dashboard" className="opacity-60 pr-2">
+          <AiFillHome className="text-lg" />
+        </NavLink>
+        <Typography className="px-2 font-LatoRegular text-gray-600 capitalize">
+          { currentPage }
+        </Typography>
+      </Breadcrumbs>
+    </div>
+  );
+};
+
+export default BreadcrumbLevel1;
