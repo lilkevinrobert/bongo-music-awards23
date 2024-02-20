3e070d313d7a63b67958cbfbbbb0a99acc0f1f29
cmn-lab <cnguruka2@gmail.com>
2024-02-20 22:04:55 +0300

    feat: Add Loading skeleton for table

--- client/src/components/Table/ArtistsDataTable.tsx
+++ client/src/components/Table/ArtistsDataTable.tsx
@@ -13,6 +13,7 @@ import axios from "../../api/axios.ts";
 import EditArtist from "../Forms/EditArtist.tsx";
 import useFetch from "../../hooks/useFetch.ts";
 import AddEmptyState from "../EmptyState/AddEmptyState.tsx";
+import LoadingTable from "../Loading/LoadingTable.tsx";
 // import EditArtist from "../Forms/EditArtist.tsx";
 
 interface DataRow {
@@ -51,8 +52,6 @@ const ArtistsDataTable: React.FC = () => {
     error: artistsDataError,
   }: FetchResult = useFetch(`${BASE_URL}/artists`);
 
-  console.log(artistsDataError);
-
   useEffect(() => {
     // Filter data based on the search term
     // const filtered = data.filter((row) => {
@@ -68,14 +67,112 @@ const ArtistsDataTable: React.FC = () => {
   return (
     <>
       {artistsDataLoading ? (
-        <p>Loading...</p>
+         <LoadingTable />
       ) : artistsDataError  ? (
         <p>network error</p>
       ) : artistsData?.data.length === 0 ? (
         <AddEmptyState itemName="artist" />
         ) : (
-          <AddEmptyState itemName="artist" />
-        // <p>data here</p>
+          <div className="mx-auto py-4">
+          <div className="flex flex-row items-center justify-between mb-4 w-full">
+            <div className="flex flex-row items-center justify-between w-auto">
+              <input
+                type="text"
+                placeholder="Search artist..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="p-4 border border-gray-500 rounded-md w-4/4 h-8 font-LatoRegular"
+              />
+              <Button
+                size="sm"
+                className="ml-2 rounded-md bg-blue-500 hover:bg-blue-700 transition-all ease-in-out flex items-center justify-center gap-2"
+                onClick={() => setSearchTerm("")}
+              >
+                <GiMagicBroom className="text-lg font-LatoRegular" />
+                Clear
+              </Button>
+            </div>
+            <Button
+              size="sm"
+              onClick={handleOpen}
+              className="flex items-center justify-center gap-2 rounded-md bg-yellow-300 hover:bg-yellow-400 transition ease-in-out text-slate-950"
+            >
+              <MdOutlinePersonAdd className="text-lg" />
+              <Typography className=" font-LatoRegular">Add</Typography>
+            </Button>
+            <Dialog
+              size="xs"
+              open={open}
+              handler={handleOpen}
+              className="bg-transparent shadow-none"
+            >
+              <div className="h-full border-red-400 flex items-center">
+                <AddArtistForm closeModal={handleOpen} />
+              </div>
+            </Dialog>
+
+            <Dialog
+              size="xs"
+              open={editOpen}
+              handler={handleEdit}
+              className="bg-transparent shadow-none"
+            >
+              <div className="h-full border-red-400 flex items-center">
+                <EditArtist closeModal={handleEdit} />
+                {/*how to pass props*/}
+              </div>
+            </Dialog>
+          </div>
+
+          <table className="table-auto w-full bg-white border shadow">
+            <thead>
+              <tr className="bg-gray-200 text-left font-LatoBold">
+                <th className="px-4 py-2">Stage Name</th>
+                <th className="px-4 py-2">Full Name</th>
+                <th className="px-4 py-2">Genre</th>
+                <th className="px-4 py-2">Phone</th>
+                <th className="px-4 py-2">Email</th>
+                <th className="px-4 py-2 text-center">Action</th>
+              </tr>
+            </thead>
+            <tbody className="font-LatoRegular text-sm">
+              {filteredData.map((row, index) => (
+                <tr
+                  key={index}
+                  className={`${
+                    index % 2 === 0 ? "bg-gray-100" : ""
+                  } group/actions`}
+                >
+                  <td className="border px-4 py-2 capitalize">
+                    {row.stage_name}
+                  </td>
+                  <td className="border px-4 py-2 capitalize">
+                    {row.fullname}
+                  </td>
+                  <td className="border px-4 py-2 capitalize">{row.genre}</td>
+                  <td className="border px-4 py-2 capitalize">{row.phone}</td>
+                  <td className="border px-4 py-2 lowercase">{row.email}</td>
+                  <td className="border px-4 py-2 opacity-80 transition-all ease-linear group-hover/actions:block">
+                    <NavLink to={`../artists/${row.stage_name}`}>
+                      <button className="bg-transparent px-2 py-1 rounded mr-1 hover:bg-blue-700 group">
+                        <MdOutlineRemoveRedEye className="w-5 h-5 text-blue-500 group-hover:text-white transition ease-in-out" />
+                      </button>
+                    </NavLink>
+                    <button
+                      className="bg-transparent px-2 py-1 rounded mr-1 hover:bg-green-700 group"
+                      onClick={handleEdit}
+                    >
+                      <MdOutlineEdit className="w-5 h-5 text-green-500 group-hover:text-white transition ease-in-out" />
+                    </button>
+                    <button className="bg-transparent px-2 py-1 rounded hover:bg-red-700 group">
+                      <MdOutlineDeleteOutline className="w-5 h-5 text-red-500 group-hover:text-white transition ease-in-out" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
     </>
   );
