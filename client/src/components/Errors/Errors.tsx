e17980dcde2d0768c98b252818ac8757f30081e3
cmn-lab <cnguruka2@gmail.com>
2024-02-21 00:48:00 +0300

    style: complete error component UI

--- client/src/components/Errors/Errors.tsx
+++ client/src/components/Errors/Errors.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { LuMusic } from "react-icons/lu";
 import { RiEmotionSadLine } from "react-icons/ri";
+import { MdOutlineArrowBack } from "react-icons/md";
+import { Button, Typography } from "@material-tailwind/react";
+import { useNavigate } from "react-router-dom";
 
 interface ErrorsProps {
   errorName: string;
@@ -9,35 +12,49 @@ interface ErrorsProps {
 const Errors: React.FC<ErrorsProps> = ({ errorName }) => {
   let errorMessage: string;
 
+  const navigate = useNavigate();
+
   switch (errorName) {
     case "TypeError":
-      errorMessage = "A type error occurred.";
+      errorMessage = "Oops! It looks like something went wrong.";
       break;
     case "ReferenceError":
-      errorMessage = "A reference error occurred.";
+      errorMessage = "Problem while processing your request.";
       break;
     case "SyntaxError":
-      errorMessage = "An error on our end!";
+      errorMessage = "A problem on our end!";
       break;
     default:
-      errorMessage = "An error occurred.";
+      errorMessage = "An unexpected error occurred.";
       break;
   }
 
   return (
     <div className="w-full h-96 flex flex-col items-center justify-center my-4 relative">
-      <LuMusic className="text-7xl text-purple-100 motion-safe:animate-ping absolute top-0 left-1/2" />
-      <LuMusic className="text-7xl text-lime-100 motion-safe:animate-ping absolute top-16 left-1/4 ml-10" />
-      <LuMusic className="text-7xl text-stone-100 motion-safe:animate-ping absolute top-16 right-1/4 mr-16" />
-      <LuMusic className="text-7xl text-blue-100 motion-safe:animate-ping absolute top-1/2 left-1/3" />
-      <LuMusic className="text-7xl text-orange-100 motion-safe:animate-ping absolute bottom-0 left-1/2 ml-10 mt-4" />
-      <LuMusic className="text-7xl text-green-100 motion-safe:animate-ping absolute top-1/2 right-1/4" />
+      <LuMusic className="text-7xl text-purple-100 motion-safe:animate-pulse absolute top-0 left-1/2" />
+      <LuMusic className="text-7xl text-lime-100 motion-safe:animate-pulse absolute top-16 left-1/4 ml-10" />
+      <LuMusic className="text-7xl text-stone-100 motion-safe:animate-pulse absolute top-16 right-1/4 mr-16" />
+      <LuMusic className="text-7xl text-blue-100 motion-safe:animate-pulse absolute top-1/2 left-1/3" />
+      <LuMusic className="text-7xl text-orange-100 motion-safe:animate-pulse absolute bottom-0 left-1/2 ml-10 mt-4" />
+      <LuMusic className="text-7xl text-green-100 motion-safe:animate-pulse absolute top-1/2 right-1/4" />
       <div className="flex flex-col items-center justify-center gap-4 z-10">
         <RiEmotionSadLine className="text-7xl text-gray-400 motion-safe:animate-bounce" />
         <div className="flex flex-col items-center">
-          <div className="text-gray-400 font-LatoBold">{errorMessage}</div>
-          <div className="text-gray-400 font-LatoRegular">
+          <Typography className="text-gray-400 font-LatoBold">
+            {errorMessage}
+          </Typography>
+          <Typography className="text-gray-400 font-LatoRegular">
             Please Try again later.
+          </Typography>
+          <div className="py-4">
+            <Button
+              size="sm"
+              onClick={() => navigate(-1)}
+              className="capitalize flex flex-row items-center gap-2"
+            >
+              <MdOutlineArrowBack className="text-lg" />
+              <span className="font-LatoRegular">Go back</span>
+            </Button>
           </div>
         </div>
       </div>
