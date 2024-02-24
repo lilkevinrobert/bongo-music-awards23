3ee738218aa3c10185bf18df110b374763c975e8
cmn-lab <cnguruka2@gmail.com>
2024-02-24 07:08:18 +0300

    style: Polish the loading profile a bit

--- client/src/components/Loading/LoadingProfile.tsx
+++ client/src/components/Loading/LoadingProfile.tsx
@@ -6,7 +6,12 @@ const LoadingProfile = () => {
         <div className="px-12 py-3 bg-gray-300 rounded animate-pulse"></div>
       </div>
       <div className="grid grid-cols-3 gap-4 my-4">
-        <div className="w-auto py-24 bg-gray-300 rounded animate-pulse"></div>
+        <div className="w-full h-fit space-y-4">
+
+        <div className="w-auto py-3 bg-gray-300 rounded animate-pulse"></div>
+        <div className="w-auto py-3 bg-gray-300 rounded animate-pulse"></div>
+        <div className="w-auto py-56 bg-gray-300 rounded animate-pulse"></div>
+        </div>
         <div className="w-auto space-y-4">
           <div className="w-auto py-40 bg-gray-300 rounded animate-pulse"></div>
           <div className="w-auto py-24 bg-gray-300 rounded animate-pulse"></div>
