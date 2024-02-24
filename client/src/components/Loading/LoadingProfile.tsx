fd78225b126a37630ab83cead28e44944b8a7365
cmn-lab <cnguruka2@gmail.com>
2024-02-24 06:58:48 +0300

    feat: Add Loading profile component

---
+++ client/src/components/Loading/LoadingProfile.tsx
@@ -0,0 +1,23 @@
+const LoadingProfile = () => {
+  return (
+    <div className="w-full h-96 bg-transparent px-4">
+      <div className="flex flex-row items-center justify-between">
+        <div className="px-16 py-3 bg-gray-300 rounded animate-pulse"></div>
+        <div className="px-12 py-3 bg-gray-300 rounded animate-pulse"></div>
+      </div>
+      <div className="grid grid-cols-3 gap-4 my-4">
+        <div className="w-auto py-24 bg-gray-300 rounded animate-pulse"></div>
+        <div className="w-auto space-y-4">
+          <div className="w-auto py-40 bg-gray-300 rounded animate-pulse"></div>
+          <div className="w-auto py-24 bg-gray-300 rounded animate-pulse"></div>
+        </div>
+        <div className="w-auto space-y-4">
+          <div className="w-auto py-40 bg-gray-300 rounded animate-pulse"></div>
+          <div className="w-auto py-24 bg-gray-300 rounded animate-pulse"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingProfile;
--- client/src/pages/Admin/JudgePage.tsx
+++ client/src/pages/Admin/JudgePage.tsx
@@ -2,201 +2,67 @@ import React from "react";
 import Layout from "../../components/Layout/Layout";
 import { Button, Card, Typography } from "@material-tailwind/react";
 import BreadcrumbLevel2 from "../../components/Breadcrumbs/BreadcrumbLevel2";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import Errors from "../../components/Errors/Errors";
+import AddEmptyState from "../../components/EmptyState/AddEmptyState";
+import LoadingProfile from "../../components/Loading/LoadingProfile";
 
-interface judgeData {
-  firstname: string;
-  middlename: string;
-  lastname: string;
-  phone: string;
+type Data = {
+  id: number;
+  profile_image_url: string;
   organization: string;
   position: string;
   expertise: string;
-  event: string;
+  phone_number: string;
   role: string;
   bio: string;
+  user_id: number;
+  event_id: number;
+  created_at: string;
+  updated_at: string;
+}
+
+interface JudgeData {
+  data: [];
+}
+interface FetchResult {
+  data: JudgeData | null;
+  loading: boolean;
+  error: Error | null;
 }
 
 const JudgePage: React.FC = () => {
-  // fetch data here
-  const data: judgeData = {
-    firstname: "john",
-    middlename: "francis",
-    lastname: "doe",
-    phone: "+255 714 540 218",
-    organization: "bma company ltd",
-    position: "ceo",
-    expertise: "music industry",
-    event: "bongo music awards 2024",
-    role: "judge",
-    bio: "a seasoned music industry professional with over two decades of experience. As an A&R executive at Warner Chappell, he has discovered and nurtured emerging talent, helping artists reach their full potential. John's passion lies in bridging the gap between creativity and business acumen, ensuring artists thrive in a competitive landscape.",
-  };
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+  const {judgeId} = useParams();
+
+    // Get data
+    const {
+      data: judgeData,
+      loading: judgeDataLoading,
+      error: judgeDataError,
+    }: FetchResult = useFetch(`${BASE_URL}/judges/${judgeId}`);
   return (
     <Layout>
       <BreadcrumbLevel2 previousPage="judges" currentPage="judge" />
-      <div className="text-slate-900 px-4 h-auto">
-        <Typography variant="h5" className="capitalize">
-          edit judge details
-        </Typography>
-        <div className="w-full h-full my-4 flex flex-col md:flex-row gap-4">
-          {/* Personal Information */}
-          <Card className="w-full h-fit md:w-1/4 px-6 py-8 bg-white bg-opacity-25 backdrop-filter backdrop-blur-md border border-gray-300 rounded-lg">
-            <Typography className="capitalize pb-4">
-              Personal information
-            </Typography>
-            <div className="flex flex-col gap-4">
-              <div className="flex flex-col">
-                <label className="mb-2 font-LatoBold capitalize">
-                  First name
-                </label>
-                <input
-                  type="text"
-                  name="firstname"
-                  className="h-10 border-slate-300 rounded-lg font-LatoRegular pl-4"
-                  value={data.firstname}
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="mb-2 font-LatoBold capitalize">
-                  middle name
-                </label>
-                <input
-                  type="text"
-                  name="firstname"
-                  className="h-10 border-slate-300 rounded-lg font-LatoRegular pl-4"
-                  value={data.firstname}
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="mb-2 font-LatoBold capitalize">surname</label>
-                <input
-                  type="text"
-                  name="lastname"
-                  className="h-10 border-slate-300 rounded-lg font-LatoRegular pl-4"
-                  value={data.lastname}
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="mb-2 font-LatoBold capitalize">Role</label>
-                <input
-                  type="text"
-                  name="role"
-                  className="h-10 border-slate-300 rounded-lg font-LatoRegular pl-4"
-                  value={data.role}
-                />
-              </div>
-              <div className="py-2">
-                <Button
-                  size="sm"
-                  fullWidth
-                  className="bg-yellow-500 capitalize transition ease-in-out hover:bg-yellow-700"
-                >
-                  saves changes
-                </Button>
-              </div>
-            </div>
-          </Card>
+      {
+        judgeDataLoading ? (
+          <p className="text-lg text-slate-900">Loading profile...</p>
+        ) : judgeDataError ? (
+          <Errors errorName={judgeDataError?.name} />
+        ) : judgeData?.data.length === 0 ? (
+          <AddEmptyState itemName="judge" />
+        ) : (
+          <LoadingProfile />
 
-          {/* Contact Informtion */}
-          <Card className="bg-white h-fit px-6 bg-opacity-25 backdrop-filter backdrop-blur-md border border-gray-300 rounded-lg">
-            <Typography className="capitalize px-0 py-8">
-              Contact information
-            </Typography>
-            <div className="flex flex-col gap-4">
-              <div className="flex flex-col">
-                <label className="mb-2 font-LatoBold capitalize">
-                  organization
-                </label>
-                <input
-                  type="text"
-                  name="organization"
-                  className="h-10 border-slate-300 rounded-lg font-LatoRegular pl-4"
-                  value={data.organization}
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="mb-2 font-LatoBold capitalize">
-                  position
-                </label>
-                <input
-                  type="text"
-                  name="position"
-                  className="h-10 border-slate-300 rounded-lg font-LatoRegular pl-4"
-                  value={data.position}
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="mb-2 font-LatoBold capitalize">phone</label>
-                <input
-                  type="text"
-                  name="phone"
-                  className="h-10 border-slate-300 rounded-lg font-LatoRegular pl-4"
-                  value={data.phone}
-                />
-              </div>
-              <div className="py-2 mb-4">
-                <Button
-                  size="sm"
-                  fullWidth
-                  className="bg-yellow-500 capitalize transition ease-in-out hover:bg-yellow-700"
-                >
-                  saves changes
-                </Button>
-              </div>
-            </div>
-          </Card>
-
-          {/* Bio & Expertise */}
-          <div className="w-full flex flex-col gap-4">
-            <Card className="bg-white h-fit px-6 py-8 bg-opacity-25 backdrop-filter backdrop-blur-md border border-gray-300 rounded-lg">
-              <Typography>Bio</Typography>
-              <textarea
-                className="rounded mt-4 border-slate-300 font-LatoRegular"
-                rows={4} cols={50}
-              >
-                {data.bio}
-              </textarea>
-              <div className="pt-4">
-                <Button
-                  size="sm"
-                  className="bg-yellow-500 float-right capitalize transition ease-in-out hover:bg-yellow-700"
-                >
-                  saves changes
-                </Button>
-              </div>
-            </Card>
-            <Card className="bg-white h-fit px-6 py-8 bg-opacity-25 backdrop-filter backdrop-blur-md border border-gray-300 rounded-lg flex flex-col gap-4">
-              <div className="flex flex-col">
-                <label className="mb-2 font-LatoBold capitalize">
-                  expertise
-                </label>
-                <input
-                  type="text"
-                  name="expertise"
-                  className="h-10 border-slate-300 rounded-lg font-LatoRegular pl-4"
-                  value={data.expertise}
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="mb-2 font-LatoBold capitalize">event</label>
-                <input
-                  type="text"
-                  name="event"
-                  className="h-10 border-slate-300 rounded-lg font-LatoRegular pl-4"
-                  value={data.event}
-                />
-              </div>
-              <div className="pt-4">
-                <Button
-                  size="sm"
-                  className="bg-yellow-500 float-right capitalize transition ease-in-out hover:bg-yellow-700"
-                >
-                  saves changes
-                </Button>
-              </div>
-            </Card>
-          </div>
-        </div>
-      </div>
+      // <div className="text-slate-900 px-4 h-auto">
+      //   <Typography variant="h5" className="capitalize">
+      //     edit judge details
+      //   </Typography>
+      //   table here
+      // </div>
+        )
+      }
     </Layout>
   );
 };
