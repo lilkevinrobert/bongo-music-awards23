d301ac4590858980f1c12cb2c868d47b9b03fbda
cmn-lab <cnguruka2@gmail.com>
2024-03-10 22:50:38 +0300

    fix: Change layout for the artist sidebar

--- client/src/components/Layout/ArtistLayout.tsx
+++ client/src/components/Layout/ArtistLayout.tsx
@@ -1,15 +1,22 @@
 import React, { ReactNode } from "react";
 import ArtistSidebar from "../navbar/ArtistSidebar.tsx";
+import { Typography } from "@material-tailwind/react";
+import ProfileMenu from "../Menu/ProfileMenu.tsx";
 interface LayoutProps {
-    children: ReactNode;
+  children: ReactNode;
 }
-const ArtistLayout:React.FC<LayoutProps> = ({ children }) => {
-    return (
-        <div className="h-screen w-full bg-slate-50 flex flex-row">
-            <ArtistSidebar />
-            <section className="w-full bg-transparent">{children}</section>
+const ArtistLayout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="h-screen w-full bg-slate-50 flex flex-row">
+      <div className="bg-transparent w-full">
+        <div className="bg-transparent flex flex-row items-center justify-between">
+          <ArtistSidebar />
+          <ProfileMenu />
         </div>
-    );
+      <section className="w-full bg-transparent">{children}</section>
+      </div>
+    </div>
+  );
 };
 
 export default ArtistLayout;
