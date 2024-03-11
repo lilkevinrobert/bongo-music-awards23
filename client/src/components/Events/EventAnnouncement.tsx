65fda738b5fc678bfd158749c1934f9cc01874a2
cmn-lab <cnguruka2@gmail.com>
2024-03-11 21:00:27 +0300

    build: Initial design for artist page

--- client/src/components/Events/EventAnnouncement.tsx
+++ client/src/components/Events/EventAnnouncement.tsx
@@ -1,28 +1,39 @@
-import { Card, Typography } from "@material-tailwind/react";
+import { Button, Card, Typography } from "@material-tailwind/react";
 import { HiHandRaised } from "react-icons/hi2";
 
 const EventAnnouncement = () => {
-    const announcement = {
-        title: "Bongo music awards 2024",
-        catchPhrase: "Hii imeenda!",
-        description: "Tunayo furaha kuwajulisha kuwa Bongo Music Awards 2024 itaanza tarehe 24/04/2024. Hivyo nyote mnakaribishwa kushiriki katika kupiga kura zenu mpaka hapo tarehe 10/05/2024.",
-    }
+  const announcement = {
+    title: "Bongo music awards 2024",
+    catchPhrase: "Hii imeenda!",
+    description:
+      "Tunayo furaha kuwajulisha kuwa Bongo Music Awards 2024 itaanza tarehe 24/04/2024. Hivyo nyote mnakaribishwa kushiriki katika kupiga kura zenu mpaka hapo tarehe 10/05/2024.",
+  };
   return (
     <>
-      <Card className="w-full p-6 border border-dashed border-rose-500 rounded-md flex flex-row items-center justify-between gap-4 bg-rose-200">
-        <div className="flex flex-col items-center">
+      <Card
+        className="w-full p-6 border border-dashed border-rose-500 rounded-md flex flex-row items-center justify-between gap-4 bg-rose-200"
+        id="announcement"
+      >
+        <div className="hidden md:flex flex-col items-center">
           <HiHandRaised className="text-4xl text-rose-500 animate-bounce" />
           <Typography>Habari!</Typography>
         </div>
-        <div className="">
-          <div className="flex flex-row items-center gap-2 text-rose-600">
-          <Typography className="font-LatoBold text-2xl">{ announcement.title }</Typography>
-          <Typography>|</Typography>
-          <Typography className="font-LatoBold text-2xl">{ announcement.catchPhrase }</Typography>
+        <div>
+          <div className="flex flex-col md:flex-row items-center gap-2 text-rose-600">
+            <Typography className="font-LatoBold text-xl md:text-xl lg:text-2xl text-center md:text-start">
+              {announcement.title}
+            </Typography>
+            <Typography className="hidden md:block">|</Typography>
+            <Typography className="font-LatoBold text-xl md:text-xl lg:text-2xl truncate">
+              {announcement.catchPhrase}
+            </Typography>
           </div>
-          <Typography className="font-LatoRegular">{announcement.description}</Typography>
+          <Typography className="font-LatoRegular">
+            {announcement.description}
+          </Typography>
         </div>
       </Card>
+      <Button size="sm" className={`rounded-md capitalize my-4 transition ease-in-out text-rose-50 bg-rose-600 hover:bg-rose-800 font-LatoRegular w-full md:w-auto`}>Go to current event</Button>
     </>
   );
 };
