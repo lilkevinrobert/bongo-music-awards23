7b0c1396021dc637985df688ee6dbaf220d6a0f5
cmn-lab <cnguruka2@gmail.com>
2024-03-11 13:43:25 +0300

    fix: Add day resolver to DateToday

--- client/src/components/Date/DateToday.tsx
+++ client/src/components/Date/DateToday.tsx
@@ -3,11 +3,31 @@ import { Card, Typography } from "@material-tailwind/react";
 const DateToday = () => {
   const date = new Date();
   const today = date.toLocaleDateString();
+  const day = date.getDay();
+
+  function resolveDay(day: number){
+    switch(day){
+      case 1:
+        return "monday";
+      case 2:
+        return "tuesday";
+      case 3:
+        return "wednesday";
+      case 4:
+        return "thursday";
+      case 5:
+        return "friday";
+      case 6:
+        return "saturday";
+      default:
+        return "sunday";
+    }
+  }
   return (
     <Card className="py-4 px-8 my-2 flex flex-col-reverse md:flex-row-reverse items-center justify-between rounded-md">
       <div className="flex flex-col">
         <Typography className="text-base font-LatoRegular">
-          Today is <span className="font-LatoBold">Monday</span>
+          Today is <span className="font-LatoBold capitalize">{ resolveDay(day) }</span>
         </Typography>
         <Typography className="text-gray-400 font-LatoRegular self-center md:self-end">
           {today}
