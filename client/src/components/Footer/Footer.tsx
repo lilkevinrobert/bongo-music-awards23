39b7985e6c82d93a2b8095f8c6f18d85b1009a39
cmn-lab <cnguruka2@gmail.com>
2024-07-06 01:05:48 +0300

    style: Update font style on Footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -29,11 +29,11 @@ const Footer = () => {
   const renderLinks = () => {
     return LINKS.map((linkGroup, index) => (
       <div key={index}>
-        <Typography variant="small" className="mb-3 font-medium opacity-50 text-slate-900 uppercase">{linkGroup.title}</Typography>
+        <Typography variant="small" className="mb-3 font-LatoBold opacity-50 text-slate-900 uppercase">{linkGroup.title}</Typography>
         <ul>
           {linkGroup.items.map((item, itemIndex) => (
             <li key={itemIndex} className="mb-2">
-              <a href={getHref(linkGroup.title, item)} className="text-sm text-slate-950 opacity-80 transition ease-in-out hover:underline hover:underline-offset-4">
+              <a href={getHref(linkGroup.title, item)} className="text-sm text-slate-950 font-LatoBold capitalize opacity-80 transition ease-in-out hover:underline hover:underline-offset-4">
                 {item}</a>
             </li>
           ))}
@@ -71,24 +71,24 @@ const Footer = () => {
       <div className="mx-auto w-full max-w-7xl py-8">
         <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2">
           <div className="bg-transprent flex flex-col items-center md:items-start justify-center">
-            <Typography variant="h5" className="mb-6 text-slate-900">
+            <Typography variant="h5" className="mb-6 text-slate-900 font-LatoBold">
               Bongo Music Awards
             </Typography>
             <div className="md:self-start font-LatoRegular text-shadow-md antialiased flex flex-col py-4 gap-2">
               <div className="flex items-center justify-start gap-4">
-                <MdPhone className="text-2xl text-slate-950 opacity-70" />
+                <MdPhone className="text-2xl text-slate-950 opacity-50" />
                 <Typography className="text-slate-950 opacity-70">
                   +255 714 456 680
                 </Typography>
               </div>
               <div className="flex items-center justify-start gap-4">
-                <HiMiniGlobeAlt className="text-2xl text-slate-950 opacity-70" />
+                <HiMiniGlobeAlt className="text-2xl text-slate-950 opacity-50" />
                 <Typography className="text-slate-950 opacity-70">
                   josia@bma.com
                 </Typography>
               </div>
               <div className="flex items-center justify-start gap-4">
-                <MdLocationPin className="text-2xl text-slate-950 opacity-70" />
+                <MdLocationPin className="text-2xl text-slate-950 opacity-50" />
                 <Typography className="text-slate-950 opacity-70">
                   Bagamoyo Rd, Dar es Salaam
                 </Typography>
@@ -102,11 +102,11 @@ const Footer = () => {
         <div className="mt-12 flex w-full flex-col items-center justify-center border-t border-blue-gray-50 py-4 md:flex-row md:justify-between">
           <Typography
             variant="small"
-            className="mb-4 text-center font-normal text-slate-500 md:mb-0"
+            className="mb-4 text-center font-LatoRegular text-slate-500 md:mb-0"
           >
             &copy; {currentYear}{" "}
             <a href="/">Bongo Music Awards</a>. All
-            Rights Reserved.
+            rights reserved.
           </Typography>
           <div className="flex gap-4 text-slate-800 sm:justify-center">
             <Typography
