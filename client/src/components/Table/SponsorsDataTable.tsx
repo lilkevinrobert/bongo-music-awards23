064536abcee70e3c48e2b3dca3696c25280eb958
cmn-lab <cnguruka2@gmail.com>
2024-07-08 00:05:08 +0300

    feat: Add sponsors table

---
+++ client/src/components/Table/SponsorsDataTable.tsx
@@ -0,0 +1,131 @@
+import { Button } from "@material-tailwind/react";
+import { useEffect, useState } from "react";
+import {
+  MdOutlineEdit,
+  MdOutlineRemoveRedEye,
+  MdOutlineDeleteOutline,
+} from "react-icons/md";
+import { NavLink } from "react-router-dom";
+
+interface DataRow {
+    id: string;
+    name: string;
+    logo: string;
+    link: string;
+
+}
+
+const SponsorsDataTable = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filteredData, setFilteredData] = useState<DataRow[]>([]);
+
+
+  const data = [
+    {
+      id: "ewew242sd",
+      name: "sponsor one",
+      logo: "link to sponsor logo",
+      link: "link to sponsor site",
+    },
+    {
+      id: "ewew242sd",
+      name: "sponsor two",
+      logo: "link to sponsor logo",
+      link: "link to sponsor site",
+    },
+    {
+      id: "ewew242sd",
+      name: "sponsor three",
+      logo: "link to sponsor logo",
+      link: "link to sponsor site",
+    },
+    {
+      id: "ewew242sd",
+      name: "sponsor four",
+      logo: "link to sponsor logo",
+      link: "link to sponsor site",
+    },
+  ];
+
+  useEffect(() => {
+    // Filter data based on the search term
+    const filtered =
+      data.filter((row) => {
+        return Object.values(row).some(
+          (value) =>
+            typeof value === "string" &&
+            value.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      }) ?? [];
+    setFilteredData(filtered);
+  }, [searchTerm, data]);
+  return (
+    <>
+      <div className="flex flex-row items-center justify-between w-fit mb-2">
+        <input
+          type="text"
+          placeholder="Search artist..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="p-4 border border-gray-500 rounded-full w-4/4 h-8 font-LatoRegular text-gray-900"
+        />
+        <Button
+          size="sm"
+          className="ml-2 rounded-full font-LatoRegular bg-blue-500 hover:bg-blue-700 transition-all ease-in-out flex items-center justify-center gap-2"
+          onClick={() => setSearchTerm("")}
+        >
+          Clear
+        </Button>
+      </div>
+      <table className="table-auto w-full bg-white border shadow">
+        <thead>
+          <tr className="bg-gray-200 text-left text-gray-900 font-LatoBold">
+            <th className="px-4 py-1">Name</th>
+            <th className="px-4 py-1">Logo</th>
+            <th className="px-4 py-1">Link</th>
+            <th className="px-4 py-1 text-center w-40"></th>
+          </tr>
+        </thead>
+        <tbody className="font-LatoRegular text-sm text-gray-800">
+          {filteredData.map((row, index) => (
+            <tr
+              key={index}
+              className={`${index % 2 === 0 ? "bg-gray-50" : ""} group/actions`}
+            >
+              <td className="border px-4 py-1 capitalize">{row.name}</td>
+              <td className="border px-4 py-1 capitalize font-normal">
+                <img
+                  src={row.logo}
+                  alt={row.name}
+                  className="w-full h-40 bg-amber-200"
+                />
+              </td>
+              <td className="border px-4 py-1 capitalize">{row.link}</td>
+              <td className="border px-4 py-1 opacity-80 transition-all ease-linear group-hover/actions:block">
+                {/* <NavLink to={`../artists/${row.id}`}>
+                <button className="bg-transparent px-2 py-1 rounded mr-1 hover:bg-blue-700 group">
+                  <MdOutlineRemoveRedEye className="w-5 h-5 text-blue-500 group-hover:text-white transition ease-in-out" />
+                </button>
+              </NavLink> */}
+                <button
+                  className="bg-transparent px-2 py-1 rounded-full mr-1 hover:bg-green-700 group"
+                  //   onClick={handleEdit}
+                >
+                  <MdOutlineEdit className="w-5 h-5 text-green-500 group-hover:text-white transition ease-in-out" />
+                </button>
+                <button
+                  //   onClick={() => handleConfirmDelete(row)}
+                  className="bg-transparent px-2 py-1 rounded-full hover:bg-red-700 group"
+                >
+                  <MdOutlineDeleteOutline className="w-5 h-5 text-red-500 group-hover:text-white transition ease-in-out" />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default SponsorsDataTable;
--- client/src/pages/Admin/AdminSponsorsPage.tsx
+++ client/src/pages/Admin/AdminSponsorsPage.tsx
@@ -1,14 +1,19 @@
-import { Button, Dialog, DialogBody, Typography } from "@material-tailwind/react";
+import {
+  Button,
+  Dialog,
+  DialogBody,
+  Typography,
+} from "@material-tailwind/react";
 import BreadcrumbLevel1 from "../../components/Breadcrumbs/BreadcrumbLevel1";
 import Layout from "../../components/Layout/Layout";
 import { IoAdd } from "react-icons/io5";
 import { useState } from "react";
 import AddSponsorForm from "../../components/Forms/AddSponsorForm";
-
+import SponsorsDataTable from "../../components/Table/SponsorsDataTable";
 
 const AdminSponsorsPage = () => {
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(!open);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(!open);
 
   return (
     <Layout>
@@ -18,16 +23,19 @@ const AdminSponsorsPage = () => {
           sponsors
         </Typography>
         <Button
-              size="sm"
-              onClick={handleOpen}
-              className="capitalize rounded-full flex flex-row items-center gap-3 font-LatoRegular bg-gray-800 hover:bg-yellow-300 hover:text-gray-900 transition ease-in-out"
-            >
-              <IoAdd className="text-lg" /> new
-            </Button>
+          size="sm"
+          onClick={handleOpen}
+          className="capitalize rounded-full flex flex-row items-center gap-3 font-LatoRegular bg-gray-800 hover:bg-yellow-300 hover:text-gray-900 transition ease-in-out"
+        >
+          <IoAdd className="text-lg" /> new
+        </Button>
+      </div>
+      <div className="bg-transparent px-4 py-4">
+        <SponsorsDataTable />
       </div>
 
-            {/* Dialogs */}
-            <Dialog
+      {/* Dialogs */}
+      <Dialog
         open={open}
         handler={handleOpen}
         className="bg-transparent m-0 rounded-none"
