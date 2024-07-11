4fdd83bd2ca99dc763a8f43a5178a8107d7aaa6c
cmn-lab <cnguruka2@gmail.com>
2024-07-11 09:23:32 +0300

    style: Change font for Loading component

--- client/src/components/Loading/Loading.tsx
+++ client/src/components/Loading/Loading.tsx
@@ -8,7 +8,7 @@ const Loading = () => {
     <div className="w-full h-screen flex flex-col items-center justify-center gap-5 bg-gray-800">
         <FaSpinner className="motion-safe:animate-spin text-yellow-100 text-3xl" />
       <div className="rounded-md p-4 max-w-sm w-full mx-auto flex flex-row items-center justify-center gap-4">
-        <Typography className="uppercase text-3xl text-yellow-100 font-bold">
+        <Typography className="uppercase text-3xl text-yellow-100 font-LatoBold">
           bongo music awards
         </Typography>
         <div className="block">
