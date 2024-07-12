ef0ce8694555b60bbbcf4a0b399fe9bdd01b9f11
cmn-lab <cnguruka2@gmail.com>
2024-07-12 19:58:46 +0300

    feat: Add link to home from footer logo

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import { BsFacebook, BsInstagram } from "react-icons/bs";
 import { HiMiniGlobeAlt } from "react-icons/hi2";
 import { MdPhone, MdLocationPin } from "react-icons/md";
 import Logo from "/logo.png";
+import { NavLink } from "react-router-dom";
 
 const LINKS = [
   {
@@ -30,12 +31,21 @@ const Footer = () => {
   const renderLinks = () => {
     return LINKS.map((linkGroup, index) => (
       <div key={index}>
-        <Typography variant="small" className="mb-3 font-LatoBold opacity-50 text-gray-950 uppercase">{linkGroup.title}</Typography>
+        <Typography
+          variant="small"
+          className="mb-3 font-LatoBold opacity-50 text-gray-950 uppercase"
+        >
+          {linkGroup.title}
+        </Typography>
         <ul>
           {linkGroup.items.map((item, itemIndex) => (
             <li key={itemIndex} className="mb-2">
-              <a href={getHref(linkGroup.title, item)} className="text-sm text-gray-950 font-LatoBold capitalize transition ease-in-out hover:underline hover:underline-offset-4">
-                {item}</a>
+              <a
+                href={getHref(linkGroup.title, item)}
+                className="text-sm text-gray-950 font-LatoBold capitalize transition ease-in-out hover:underline hover:underline-offset-4"
+              >
+                {item}
+              </a>
             </li>
           ))}
         </ul>
@@ -46,27 +56,27 @@ const Footer = () => {
   const getHref = (title: string, item: string) => {
     switch (title) {
       case "About":
-        if(item == "Bongo Music Awards"){
+        if (item == "Bongo Music Awards") {
           return "/about";
         }
-        if(item == "Contacts"){
+        if (item == "Contacts") {
           return "/contact";
         }
         return "/about";
       case "Follow us":
-        if(item == "Instagram"){
-            return social_links.instagram
+        if (item == "Instagram") {
+          return social_links.instagram;
         }
-        if(item == "Facebook"){
-            return social_links.facebook
+        if (item == "Facebook") {
+          return social_links.facebook;
         }
         return `/`;
       case "Legal":
-        if(item == "Privacy Policy"){
-            return `/privacy-policy`
+        if (item == "Privacy Policy") {
+          return `/privacy-policy`;
         }
-        if(item == "Terms & Conditions"){
-            return `/terms-conditions`
+        if (item == "Terms & Conditions") {
+          return `/terms-conditions`;
         }
         return `/`;
       default:
@@ -78,7 +88,13 @@ const Footer = () => {
       <div className="mx-auto w-full max-w-7xl py-8">
         <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2">
           <div className="bg-transprent flex flex-col items-start px-6 md:px-0 md:items-start justify-center">
-            <img src={Logo} alt="bongo music awards logo" className="text-sm w-36 mb-6" />
+            <NavLink to="/">
+              <img
+                src={Logo}
+                alt="bongo music awards logo"
+                className="text-sm w-36 mb-6"
+              />
+            </NavLink>
             <div className="md:self-start font-LatoRegular text-shadow-md antialiased flex flex-col py-4 gap-2">
               <div className="flex items-center justify-start gap-4">
                 <MdPhone className="text-2xl text-gray-950 opacity-90" />
@@ -88,9 +104,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center justify-start gap-4">
                 <HiMiniGlobeAlt className="text-2xl text-gray-950 opacity-90" />
-                <Typography className="text-gray-950">
-                  josia@bma.com
-                </Typography>
+                <Typography className="text-gray-950">josia@bma.com</Typography>
               </div>
               <div className="flex items-center justify-start gap-4">
                 <MdLocationPin className="text-2xl text-gray-950 opacity-90" />
@@ -101,7 +115,7 @@ const Footer = () => {
             </div>
           </div>
           <div className="grid grid-cols-3 justify-between gap-4 bg-transparent text-base px-6 md:px-auto">
-            { renderLinks() }
+            {renderLinks()}
           </div>
         </div>
         <div className="mt-12 flex w-full flex-col items-center justify-center border-t border-blue-gray-50 py-4 md:flex-row md:justify-between">
@@ -109,9 +123,8 @@ const Footer = () => {
             variant="small"
             className="mb-4 text-center font-LatoRegular text-gray-900 opacity-80 md:mb-0"
           >
-            &copy; {currentYear}{" "}
-            <a href="/">Bongo Music Awards</a>. All
-            rights reserved.
+            &copy; {currentYear} <a href="/">Bongo Music Awards</a>. All rights
+            reserved.
           </Typography>
           <div className="flex gap-4 text-gray-900 sm:justify-center">
             <Typography
@@ -119,7 +132,7 @@ const Footer = () => {
               href={social_links.facebook}
               className="opacity-80 transition-opacity hover:opacity-100"
             >
-             <BsFacebook className="text-xl" />
+              <BsFacebook className="text-xl" />
             </Typography>
             <Typography
               as="a"
