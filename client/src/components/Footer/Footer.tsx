0f802c3115c1c9713da89e5f07960b6c1214cfc1
cmn-lab <cnguruka2@gmail.com>
2024-07-12 18:21:26 +0300

    style: Add Logo to footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import { Typography } from "@material-tailwind/react";
 import { BsFacebook, BsInstagram } from "react-icons/bs";
 import { HiMiniGlobeAlt } from "react-icons/hi2";
 import { MdPhone, MdLocationPin } from "react-icons/md";
+import Logo from "/logo.png";
 
 const LINKS = [
   {
@@ -29,11 +30,11 @@ const Footer = () => {
   const renderLinks = () => {
     return LINKS.map((linkGroup, index) => (
       <div key={index}>
-        <Typography variant="small" className="mb-3 font-LatoBold opacity-50 text-slate-900 uppercase">{linkGroup.title}</Typography>
+        <Typography variant="small" className="mb-3 font-LatoBold opacity-50 text-gray-950 uppercase">{linkGroup.title}</Typography>
         <ul>
           {linkGroup.items.map((item, itemIndex) => (
             <li key={itemIndex} className="mb-2">
-              <a href={getHref(linkGroup.title, item)} className="text-sm text-slate-950 font-LatoBold capitalize opacity-80 transition ease-in-out hover:underline hover:underline-offset-4">
+              <a href={getHref(linkGroup.title, item)} className="text-sm text-gray-950 font-LatoBold capitalize transition ease-in-out hover:underline hover:underline-offset-4">
                 {item}</a>
             </li>
           ))}
@@ -71,25 +72,23 @@ const Footer = () => {
       <div className="mx-auto w-full max-w-7xl py-8">
         <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2">
           <div className="bg-transprent flex flex-col items-start px-6 md:px-0 md:items-start justify-center">
-            <Typography variant="h5" className="mb-6 text-slate-900 font-LatoBold">
-              Bongo Music Awards
-            </Typography>
+            <img src={Logo} alt="bongo music awards logo" className="text-sm w-36 mb-6" />
             <div className="md:self-start font-LatoRegular text-shadow-md antialiased flex flex-col py-4 gap-2">
               <div className="flex items-center justify-start gap-4">
-                <MdPhone className="text-2xl text-slate-950 opacity-50" />
-                <Typography className="text-slate-950 opacity-70">
+                <MdPhone className="text-2xl text-gray-950 opacity-90" />
+                <Typography className="text-gray-950">
                   +255 714 456 680
                 </Typography>
               </div>
               <div className="flex items-center justify-start gap-4">
-                <HiMiniGlobeAlt className="text-2xl text-slate-950 opacity-50" />
-                <Typography className="text-slate-950 opacity-70">
+                <HiMiniGlobeAlt className="text-2xl text-gray-950 opacity-90" />
+                <Typography className="text-gray-950">
                   josia@bma.com
                 </Typography>
               </div>
               <div className="flex items-center justify-start gap-4">
-                <MdLocationPin className="text-2xl text-slate-950 opacity-50" />
-                <Typography className="text-slate-950 opacity-70">
+                <MdLocationPin className="text-2xl text-gray-950 opacity-90" />
+                <Typography className="text-gray-950">
                   Bagamoyo Rd, Dar es Salaam
                 </Typography>
               </div>
@@ -102,13 +101,13 @@ const Footer = () => {
         <div className="mt-12 flex w-full flex-col items-center justify-center border-t border-blue-gray-50 py-4 md:flex-row md:justify-between">
           <Typography
             variant="small"
-            className="mb-4 text-center font-LatoRegular text-slate-500 md:mb-0"
+            className="mb-4 text-center font-LatoRegular text-gray-900 opacity-80 md:mb-0"
           >
             &copy; {currentYear}{" "}
             <a href="/">Bongo Music Awards</a>. All
             rights reserved.
           </Typography>
-          <div className="flex gap-4 text-slate-800 sm:justify-center">
+          <div className="flex gap-4 text-gray-900 sm:justify-center">
             <Typography
               as="a"
               href={social_links.facebook}
