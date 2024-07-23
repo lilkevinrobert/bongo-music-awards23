de965056389b9f4c8c8cafe665d9d4afb9b6940e
cmn-lab <cnguruka2@gmail.com>
2024-07-23 04:35:33 +0300

    style: Make the loading page dark, match with logo

--- client/src/components/Loading/Loading.tsx
+++ client/src/components/Loading/Loading.tsx
@@ -5,8 +5,9 @@ import { FaSpinner } from "react-icons/fa6";
 
 const Loading = () => {
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center gap-5 bg-gray-800">
-        <FaSpinner className="motion-safe:animate-spin text-yellow-100 text-3xl" />
+    <div className="w-full h-screen flex flex-col items-center justify-center gap-5 bg-zinc-950">
+      {/* in case of any issues add "motion-safe:" to spinner animate-spin ==> motion-safe:animate-spin  */}
+        <FaSpinner className="animate-spin text-yellow-100 text-3xl" />
       <div className="rounded-md p-4 max-w-sm w-full mx-auto flex flex-row items-center justify-center gap-4">
         <Typography className="uppercase text-3xl text-yellow-100 font-LatoBold">
           bongo music awards
