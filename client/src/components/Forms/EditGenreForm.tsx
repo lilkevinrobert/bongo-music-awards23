9c073fecb1277909aad16b83e911f7eb9da4aa60
cmn-lab <cnguruka2@gmail.com>
2024-08-15 14:14:39 +0300

    feat: Add functionality for Edit Genre

--- client/src/components/Forms/EditGenreForm.tsx
+++ client/src/components/Forms/EditGenreForm.tsx
@@ -1,45 +1,97 @@
-import { Button, Card, CardBody, CardHeader, CardFooter, Typography } from "@material-tailwind/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  Typography,
+} from "@material-tailwind/react";
 import { IGenre } from "../Genre/Genres";
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 interface FormProps {
   closeModal: () => void;
   genre: IGenre | null;
 }
 
+interface FormData {
+  id: any;
+  name: string;
+}
+
 const EditGenreForm = ({ closeModal, genre }: FormProps) => {
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+  const [genreData, setGenreData] = useState<FormData | null>(genre);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setGenreData((prevData) => ({
+      ...prevData!,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios
+      .patch(`${BASE_URL}/v1/genres/${genreData?.id}`, {
+        name: genreData?.name,
+      })
+      .then((response) => {
+        if (response.status == 200) {
+          closeModal();
+          toast.success("Genre Updated Successfully");
+          setTimeout(() => window.location.reload(), 3000);
+        } else {
+          closeModal();
+          toast.error("Failed to Update Genre.");
+        }
+      })
+      .catch(() => {
+        closeModal();
+        toast.error("Failed to Update Genre.");
+      });
+  };
+
   return (
     <Card className="mx-auto w-full max-w-[24rem] rounded-md">
-      <form>
-        <CardHeader className="shadow-none py-4 px-2">
-        <Typography
-          variant="h4"
-          className="text-2xl capitalize font-LatoBold text-gray-900"
-        >
-          Edit Genre
-        </Typography>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <CardHeader className="px-2 py-4 shadow-none">
+          <Typography
+            variant="h4"
+            className="font-LatoBold text-2xl capitalize text-gray-900"
+          >
+            Edit Genre
+          </Typography>
         </CardHeader>
         <CardBody className="flex flex-col gap-4">
-        <div className="flex flex-col items-start gap-2 font-LatoBold text-gray-900">
-          <Typography className="capitalize">name</Typography>
-          <input
-            type="text"
-            value={genre?.name}
-            className="w-full h-[2rem] border-gray-300 font-LatoRegular text-sm rounded"
-            placeholder="Name of Sponsor"
-          />
-        </div>
+          <div className="flex flex-col items-start gap-2 font-LatoBold text-gray-900">
+            <Typography className="capitalize">name</Typography>
+            <input
+              type="text"
+              value={genreData?.name}
+              name="name"
+              className="h-[2rem] w-full rounded border-gray-300 font-LatoRegular text-sm"
+              placeholder="Name of Genre"
+              onChange={(e) => handleChange(e)}
+              required
+            />
+          </div>
         </CardBody>
         <CardFooter className="flex flex-row items-center justify-end gap-1">
           <Button
             size="md"
             onClick={closeModal}
-            className="capitalize text-gray-900 shadow-none bg-transparent hover:bg-gray-200 transition ease-in-out font-LatoBold rounded-full"
+            className="rounded-full bg-transparent font-LatoBold capitalize text-gray-900 shadow-none transition ease-in-out hover:bg-gray-200"
           >
             cancel
           </Button>
           <Button
+            type="submit"
             size="md"
-            className="capitalize hover:text-gray-900 hover:bg-yellow-300 transition ease-in-out font-LatoRegular hover:font-LatoBold rounded-full"
+            className="rounded-full font-LatoRegular capitalize transition ease-in-out hover:bg-yellow-300 hover:font-LatoBold hover:text-gray-900"
           >
             save changes
           </Button>
--- client/src/components/Loading/LoadingItems.tsx
+++ client/src/components/Loading/LoadingItems.tsx
@@ -4,8 +4,9 @@ const LoadingItems = () => {
         <div className="flex flex-row items-center gap-2">
             <div className="py-5 px-24 rounded-full bg-slate-200 shadow-sm"></div>
             <div className="py-5 px-14 rounded-full bg-slate-200 shadow-sm"></div>
+            <div className="py-5 px-16 rounded-full bg-slate-200 shadow-sm"></div>
             <div className="py-5 px-14 rounded-full bg-slate-200 shadow-sm"></div>
-            <div className="py-5 px-14 rounded-full bg-slate-200 shadow-sm"></div>
+            <div className="py-5 px-20 rounded-full bg-slate-200 shadow-sm"></div>
         </div>
     </div>
   )
