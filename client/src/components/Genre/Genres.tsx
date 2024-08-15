b0925af8123034fa060d98e81f4d668d8add7a24
cmn-lab <cnguruka2@gmail.com>
2024-08-15 10:16:25 +0300

    feat: Get Genre Data from API

--- client/src/components/Genre/Genres.tsx
+++ client/src/components/Genre/Genres.tsx
@@ -5,22 +5,38 @@ import { TiDelete } from "react-icons/ti";
 import { AiFillEdit } from "react-icons/ai";
 import EditGenreForm from "../Forms/EditGenreForm";
 import DeleteDialog from "../Dialog/DeleteDialog";
+import useFetch from "../../hooks/useFetch";
+import Errors from "../Errors/Errors";
+import AddEmptyState from "../EmptyState/AddEmptyState";
+import LoadingItems from "../Loading/LoadingItems";
 
 export interface IGenre {
   id: any;
   name: string;
 }
 
+interface Data {
+  data: [];
+}
+
+interface FetchResult {
+  data: Data | null;
+  loading: boolean;
+  error: Error | null;
+}
+
 const Genres = () => {
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+
   // Edit dialog handling
   const [_editId, setEditId] = useState(null);
   const [editData, setEditData] = useState<IGenre | null>(null)
   const [openEditGenre, setOpenEditGenre] = useState(false);
   const handleOpenEditGenre = () => setOpenEditGenre((cur) => !cur);
-  const openEditDialogHandler = (id:any) => {
-    setEditData(genreList[id])
-    if(id){
-      setEditId(id)
+  const openEditDialogHandler = (item: IGenre) => {
+    setEditData(item)
+    if(item.id){
+      setEditId(item.id)
     }
     setOpenEditGenre((cur) => !cur)
   }
@@ -37,41 +53,33 @@ const Genres = () => {
   };
 
   // Data from API
-  const genreList = [
-    {
-      id: "dkslds1",
-      name: "hip-hop",
-    },
-    {
-      id: "dkslds2",
-      name: "bongo flava",
-    },
-    {
-      id: "dkslds3",
-      name: "singeli",
-    },
-    {
-      id: "dkslds4",
-      name: "taarabu",
-    },
-  ];
+  const { data: genreData, loading, error }: FetchResult = useFetch(`${BASE_URL}/v1/genres`)
+
   return (
     <>
-    <div className="py-2 flex flex-row flex-wrap gap-2">
-      {genreList.map((item, i) => (
-        <div
-          key={i}
-          className="group flex items-center justify-between gap-2 font-LatoBold text-xs text-gray-800 uppercase bg-amber-100 w-fit px-4 py-2 rounded-full"
-        >
-          {item.name}
-          <IoIosArrowForward className="text-lg text-gray-500 group-hover:hidden transition ease-linear" />
-          <div className="hidden group-hover:flex flex-row items-center gap-2 transition ease-linear bg-amber-50 px-2 rounded-full">
-            <AiFillEdit onClick={() => openEditDialogHandler(i)} className="text-lg text-green-500 cursor-pointer hover:border-2 hover:border-green-500 rounded-full" />
-            <TiDelete  onClick={() => deleteDialogHandler(item.id)} className="text-xl text-red-500 cursor-pointer hover:border-2 hover:border-red-500 rounded-full" />
+    {
+      loading ? (<LoadingItems />) :
+      error ? (<Errors errorName={error.name} />) :
+      genreData?.data.length === 0 ? (
+        <AddEmptyState itemName="genre" />
+      ) : (
+        <div className="py-2 flex flex-row flex-wrap gap-2">
+        {genreData?.data.map((item: IGenre, i) => (
+          <div
+            key={i}
+            className="group flex items-center justify-between gap-2 font-LatoBold text-xs text-gray-800 uppercase bg-amber-100 w-fit px-4 py-2 rounded-full"
+          >
+            {item.name}
+            <IoIosArrowForward className="text-lg text-gray-500 group-hover:hidden transition ease-linear" />
+            <div className="hidden group-hover:flex flex-row items-center gap-2 transition ease-linear bg-amber-50 px-2 rounded-full">
+              <AiFillEdit onClick={() => openEditDialogHandler(item)} className="text-lg text-green-500 cursor-pointer hover:border-2 hover:border-green-500 rounded-full" />
+              <TiDelete  onClick={() => deleteDialogHandler(item.id)} className="text-xl text-red-500 cursor-pointer hover:border-2 hover:border-red-500 rounded-full" />
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      )
+    }
 
     {/* Dialogs */}
     <Dialog
---
+++ client/src/components/Loading/LoadingItems.tsx
@@ -0,0 +1,14 @@
+const LoadingItems = () => {
+  return (
+    <div className="flex flex-row items-center justify-between my-2 animate-pulse">
+        <div className="flex flex-row items-center gap-2">
+            <div className="py-5 px-24 rounded-full bg-slate-200 shadow-sm"></div>
+            <div className="py-5 px-14 rounded-full bg-slate-200 shadow-sm"></div>
+            <div className="py-5 px-14 rounded-full bg-slate-200 shadow-sm"></div>
+            <div className="py-5 px-14 rounded-full bg-slate-200 shadow-sm"></div>
+        </div>
+    </div>
+  )
+}
+
+export default LoadingItems
