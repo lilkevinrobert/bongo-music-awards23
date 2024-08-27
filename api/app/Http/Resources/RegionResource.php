3b4b7c80d93f4abc3bbbc180ead7836f4975d4dc
Alex Yombo <lilkevinrobert@gmail.com>
2024-08-27 23:03:29 +0300

    Update region resource

--- api/app/Http/Resources/RegionResource.php
+++ api/app/Http/Resources/RegionResource.php
@@ -9,11 +9,15 @@ class RegionResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name
+        ];
     }
 }
