c71cceeab12052804ae0ccda10ffdb2f3e1ab93d
cmn-lab <cnguruka2@gmail.com>
2024-11-17 15:00:56 +0300

    feat: Add Nominations

--- client/src/components/Nomination/AdminNominationsView.tsx
+++ client/src/components/Nomination/AdminNominationsView.tsx
@@ -6,6 +6,7 @@ import { Button, Dialog, Typography } from "@material-tailwind/react";
 import useFetch from "../../hooks/useFetch";
 import EditNominationForm from "../Forms/EditNominationForm";
 import { useState } from "react";
+import { GoDotFill } from "react-icons/go";
 
 
 interface NominationData {
@@ -66,9 +67,9 @@ const AdminNominationsView = ({ awardId }: AwardNominationProps) => {
     loading: nominationDataLoading,
     error: nominationDataError
   }: NominaitonFetchResult = useFetch(`${BASE_URL}/nominations/${awardId}/status`);
-  console.log(nominationData)
   return (
     <>
+      {/* Award Data */}
       <div className="flex flex-row items-center justify-between gap-2">
         {
           awardDataLoading ? (
@@ -86,6 +87,7 @@ const AdminNominationsView = ({ awardId }: AwardNominationProps) => {
           ) : ""
         }
       </div>
+      {/* Nomination Data  */}
       <div className="">
         <div>
           {
@@ -94,32 +96,46 @@ const AdminNominationsView = ({ awardId }: AwardNominationProps) => {
                 <div className="flex flex-col gap-2">
                   {
                     nominationDataError ? (<p>A problem fetching nomination data</p>) :
-                      nominationData.data.map((nomination: any, i) => (
-                        <div key={i} className="py-6 px-2 bg-transparent">
-                          {/* Nomination Category */}
-                          <div className="flex flex-row items-center justify-between">
-                            <NavLink to={`/admin/awards/${nav.awardId}/categories/${nomination.category.id}`}>
-                              <Typography className="text-gray-800 font-LatoBold uppercase underline underline-offset-4">
-                                {nomination.category.name}
-                              </Typography>
-                            </NavLink>
-                            <FaArrowUp
-                              onClick={toTop}
-                              className="text-gray-900 animate-bounce cursor-pointer text-base hover:text-lg transition ease-linear"
-                            />
-                          </div>
-                          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2 mt-2">
-                            {
-                              nomination.artists.map((artist: any, i:any) => (
-                                <div key={i} className="bg-amber-100 py-2 px-4 rounded-md">
-                                  <Typography className="text-gray-800 text-sm font-LatoBold capitalize">{artist.record_label}</Typography>
-                                  <Typography className="text-gray-800 text-sm font-LatoRegular capitalize">~ {artist.stage_name}</Typography>
-                                </div>
-                              ))
-                            }
-                          </div>
-                        </div>
-                      ))
+                      nominationData.data.length == 0 ? <p className="text-base text-gray-700 font-LatoRegular text-center py-3 bg-gray-100 capitalize mt-4">no nomination request found.</p>
+                        : nominationData.data.map((nominationList: any[], i) => {
+                          const genreGroup = nominationList[i]?.genre;
+                          return (
+                            <div key={i} className="bg-transparent mt-2">
+                              <Typography className="font-LatoBold text-gray-900 uppercase pl-2 py-2 bg-amber-100 rounded-r-full">{genreGroup}</Typography>
+                              {
+                                nominationList.map((nomination: any, count) => (
+                                  <div key={count} className="py-2 px-2 bg-transparent">
+                                    {/* Nomination Category */}
+                                    <div className="flex flex-row items-center justify-between">
+                                      <div className="flex flex-row items-center ">
+                                        <GoDotFill />
+                                        <NavLink to={`/admin/awards/${nav.awardId}/categories/`}>
+                                          <Typography className="text-gray-800 font-LatoBold uppercase underline underline-offset-4">
+                                            {/* {nomination.category.name} */} {nomination.category}
+                                          </Typography>
+                                        </NavLink>
+                                      </div>
+                                      <FaArrowUp
+                                        onClick={toTop}
+                                        className="text-gray-900 animate-bounce cursor-pointer text-base hover:text-lg transition ease-linear"
+                                      />
+                                    </div>
+                                    <div className="hidden grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2 mt-2">
+                                      {/* {
+                                nomination.artists.map((artist: any, i: any) => (
+                                  <div key={i} className="bg-amber-100 py-2 px-4 rounded-md">
+                                    <Typography className="text-gray-800 text-sm font-LatoBold capitalize">{artist.record_label}</Typography>
+                                    <Typography className="text-gray-800 text-sm font-LatoRegular capitalize">~ {artist.stage_name}</Typography>
+                                  </div>
+                                ))
+                              } */}
+                                    </div>
+                                  </div>
+                                ))
+                              }
+                            </div>
+                          )
+                        })
                   }
                 </div>
               </>
